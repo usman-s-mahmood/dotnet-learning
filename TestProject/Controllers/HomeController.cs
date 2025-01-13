8e50c3d00a7f25345e01da6c2406852e6647ff44
UCP GPL 301 <ucp@ucp.com>
2025-01-13 09:24:25 +0500

    updating repo from GPL terminal

--- TestProject/Controllers/HomeController.cs
+++ TestProject/Controllers/HomeController.cs
@@ -13,6 +13,10 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    public IActionResult TestView() {
+        return View();
+    }
+
     public IActionResult Index()
     {
         return View();
