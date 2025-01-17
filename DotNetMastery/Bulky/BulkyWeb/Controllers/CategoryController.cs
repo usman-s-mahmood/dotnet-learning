87d0c63c93ce1f90042a89380b7241f1fdff4c1e
Usman Shahid <usmanshahid027@outlook.com>
2025-01-18 01:40:48 +0500

    did a lot about DB connection, models, data extraction, etc.

---
+++ DotNetMastery/Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController: Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList(); // var can also be used
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ DotNetMastery/Bulky/BulkyWeb/Models/Category.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; } // Primary Key
+        [Required]
+        public string? Name { get; set; } // Category Name
+        public int DisplayOrder { get; set; } // Display Order for sorting
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System.ComponentModel.DataAnnotations;
+
+//namespace BulkyWeb.Models
+//{
+//    public class Category
+//    {
+//        // [Key] // Data anotation for use if the name of primary key is not Id below
+//        public int Id { get; set; }
+//        [Required] // Required data anotation for !NULL field below
+//        public string? Name { get; set; }
+//        public int DisplayOrder { get; set; }
+//    }
+//}
