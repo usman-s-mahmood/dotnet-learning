14943d555c632c69b60a636cf561ef73e77ecd8c
Usman Shahid <usmanshahid027@outlook.com>
2025-01-25 00:19:18 +0500

    did working on Edit and Delete Controllers, CRUD operations completed for categories

--- DotNetMastery/Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ DotNetMastery/Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -58,5 +58,60 @@ namespace BulkyWeb.Controllers
             }
             return View();
         }
+        [HttpGet]
+        [Route("Category/Edit/{id?}")]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+            Console.WriteLine($"Id received is: {id}");
+            Category? categoryFromDb = _db.Categories.Find(id); // Find will find the category on the basis of primary key
+            Category? categoryFromDb1 = _db.Categories.FirstOrDefault(o => o.Id == id); // this method can find a record based on any attribute of the object!
+            Category? categoryFromDb2 = _db.Categories.Where(o => o.Id == id).FirstOrDefault(); // just another method!
+
+            if (categoryFromDb == null)
+                return NotFound();
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        [Route("Category/EditCat")]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                //Console.WriteLine(obj);
+                //Console.WriteLine($"Properties are: \nID: {obj.Id}\nName: {obj.Name}\n{obj.DisplayOrder}");
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+                return NotFound();
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            if (id == 0)
+                return NotFound();
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+                return NotFound();
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
